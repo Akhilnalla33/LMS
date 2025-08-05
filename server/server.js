a4dca4cae9d9fab9230b1cb15029f6fcee66d31b
Akhilnalla33 <166206096+Akhilnalla33@users.noreply.github.com>
2025-08-05 18:22:58 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: "https://lms-frontend-pysf.onrender.com",
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"]
+  credentials: true,
 }));
 app.options("*", cors());
 
@@ -50,4 +51,5 @@ app.get("/", (req, res) => res.send("🚀 Welcome to the LMS API!"));
 // Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
